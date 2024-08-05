243a01fb98a7b8dcb8ae79fd2bae5b274eda513e
franchesoni <marchesoniacland@gmail.com>
2024-08-06 00:52:49 +0200

    training script for monocular depth

--- monocular.py
+++ monocular.py
@@ -1,49 +1,164 @@
 from pathlib import Path
+from functools import partial
+import itertools
+from contextlib import suppress
 import time
 
 import torch
 from torch.utils.tensorboard import SummaryWriter
-import numpy as np
 import cv2
 from schedulefree import AdamWScheduleFree
 
 
+def fast_collate(batch):
+    """A fast collation function optimized for uint8 images (np array or torch) and int64 targets (labels)"""
+    batch_size = len(batch)
+    imgs = torch.zeros((batch_size, *batch[0][0].shape), dtype=torch.uint8)
+    for i in range(batch_size):
+        imgs[i] += torch.from_numpy(batch[i][0])
+    depths = torch.stack([torch.from_numpy(batch[i][1]) for i in range(batch_size)])
+    return imgs, depths
+
+
+class PrefetchLoader:
+
+    def __init__(
+        self,
+        loader,
+        gpu_transform,
+        device=torch.device("cuda"),
+    ):
+
+        self.loader = loader
+        self.device = device
+        self.is_cuda = torch.cuda.is_available() and device.type == "cuda"
+        self.transform = gpu_transform
+
+    def __iter__(self):
+        first = True
+        if self.is_cuda:
+            stream = torch.cuda.Stream()
+            stream_context = partial(torch.cuda.stream, stream=stream)
+        else:
+            stream = None
+            stream_context = suppress
+
+        for next_input, next_target in self.loader:
+
+            with stream_context():
+                next_input = next_input.to(device=self.device, non_blocking=True)
+                next_target = next_target.to(device=self.device, non_blocking=True)
+                next_input, next_target = self.transform(next_input, next_target)
+
+            if not first:
+                yield input, target
+            else:
+                first = False
+
+            if stream is not None:
+                torch.cuda.current_stream().wait_stream(stream)
+
+            input = next_input
+            target = next_target
+
+        yield input, target
+
+    def __len__(self):
+        return len(self.loader)
+
+    @property
+    def sampler(self):
+        return self.loader.sampler
+
+    @property
+    def dataset(self):
+        return self.loader.dataset
+
+
 class Scenenet(torch.utils.data.Dataset):
     # https://robotvault.bitbucket.io/scenenet-rgbd.html
-    def __init__(self, root):
+    # to download: `wget https://www.doc.ic.ac.uk/~bjm113/scenenet_data/train_split/train_0.tar.gz` (and the same with train_1.tar.gz)
+    # to extract: `tar -xvf train_0.tar.gz``
+    def __init__(self, root, sample_every=1):
         self.root = Path(root)
         print("Loading Scenet from", self.root)
         self.images = sorted(list(self.root.glob("**/photo/*.jpg")))
         self.depths = sorted(list(self.root.glob("**/depth/*.png")))
+        if sample_every > 1:
+            self.images = self.images[::sample_every]
+            self.depths = self.depths[::sample_every]
         print("Done loading, found", len(self.images), "images.")
 
     def __getitem__(self, index):
         # Load image
         imgpath, depthpath = self.images[index], self.depths[index]
-        img, depth = cv2.imread(str(imgpath)), cv2.imread(str(depthpath), cv2.IMREAD_UNCHANGED)  # read in BGR, the network doesn't care
-        img, depth = cv2.resize(img, (256, 256)), cv2.resize(depth, (256, 256))
-        img = torch.tensor((img / 255 - 0.5).astype(np.float32)).permute(2, 0, 1)  
-        invdepth = torch.tensor((1 / (1+depth)).astype(np.float32))
-        return img, invdepth
+        img, depth = cv2.imread(str(imgpath)), cv2.imread(
+            str(depthpath), cv2.IMREAD_UNCHANGED
+        )  # read in BGR, the network doesn't care
+        return img, depth
 
     def __len__(self):
         return len(self.images)
 
-def collate_fn(batch):
-    imgs, depths = zip(*batch)
-    imgs = torch.stack(imgs)
-    depths = torch.stack(depths)
-    return imgs, depths
 
-def train(max_seconds=3600, batch_size=32, lr=1e-4, beta=0.9, warmup_steps=10000, weight_decay=0.001, num_workers=12):
-    device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
-    model = torch.hub.load('intel-isl/MiDaS', 'MiDaS_small')
+def gpu_transform(img, depth):
+    img, invdepth = img.float().div(255).sub(0.5), 1 / (1 + depth.float())
+    img, invdepth = img.permute(0, 3, 1, 2), invdepth.unsqueeze(1)  # (B, C, H, W)
+    img, invdepth = torch.nn.functional.interpolate(
+        img, size=(256, 256), mode="bilinear"
+    ), torch.nn.functional.interpolate(invdepth, size=(256, 256), mode="bilinear")
+    return img, invdepth
+
+
+def train(
+    max_seconds=3600,
+    batch_size=32,
+    lr=1e-4,
+    beta=0.9,
+    warmup_steps=1000,
+    val_every=100,
+    weight_decay=0.001,
+    num_workers=96,
+):
+    # utils
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    writer = SummaryWriter(log_dir="monocular")
+    # model
+    model = torch.hub.load("intel-isl/MiDaS", "MiDaS_small")
     model = model.to(device)
-    ds = Scenenet(root='train')
-    dl = torch.utils.data.DataLoader(ds, batch_size=batch_size, shuffle=True, collate_fn=collate_fn, num_workers=num_workers)
+    # data
+    train_ds = Scenenet(root="train/0")
+    torch_dl = torch.utils.data.DataLoader(
+        train_ds,
+        batch_size=batch_size,
+        shuffle=True,
+        collate_fn=fast_collate,
+        num_workers=num_workers,
+        drop_last=True,
+    )
+    dl = PrefetchLoader(torch_dl, gpu_transform, device=device)
+    val_ds = Scenenet(root="train/1", sample_every=1000)
+    val_dl = PrefetchLoader(
+        torch.utils.data.DataLoader(
+            val_ds,
+            batch_size=batch_size,
+            shuffle=False,
+            collate_fn=fast_collate,
+            num_workers=num_workers,
+            drop_last=True,
+        ),
+        gpu_transform,
+        device=device,
+    )
+    # optim
     loss_fn = torch.nn.functional.l1_loss
-    optim = AdamWScheduleFree(model.parameters(), lr=lr, betas=(beta, 0.999), weight_decay=weight_decay, warmup_steps=warmup_steps)
-    writer = SummaryWriter(log_dir='monocular')
+    optim = AdamWScheduleFree(
+        model.parameters(),
+        lr=lr,
+        betas=(beta, 0.999),
+        weight_decay=weight_decay,
+        warmup_steps=warmup_steps,
+    )
 
     model.train()
     optim.train()
@@ -52,19 +167,56 @@ def train(max_seconds=3600, batch_size=32, lr=1e-4, beta=0.9, warmup_steps=10000
     global_step = 0
     while not (time.time() - st > max_seconds):
         for imgs, invdepths in dl:
-            imgs, invdepths = imgs.to(device, non_blocking=True), invdepths.to(device, non_blocking=True)
-            out = model(imgs)
+            out = model(imgs).unsqueeze(
+                1
+            )  # (B, 1, H, W), I don't know why it's not already 4D (has no channel dim)
             loss = loss_fn(out, invdepths)
             loss.backward()
             optim.step()
             global_step += 1
 
+            if global_step % val_every == 0:  # maybe validate
+                val_scores = validate(dl, val_dl, model, optim)
+                for score_name, score_value in val_scores.items():
+                    writer.add_scalar(f"val/{score_name}", score_value, global_step)
+                print(f"step={global_step}, validation scores: {val_scores}")
+
             time_so_far = time.time() - st
-            speed = global_step/time_so_far
-            writer.add_scalar('train/speed', speed, global_step)
-            writer.add_scalar('train/loss', loss.item(), global_step)
-            print(f"speed={speed:.2f}(steps/s), time={int(time_so_far)}(s), loss={loss.item():.3g}", end='\r')
-    
+            speed = global_step / time_so_far
+            writer.add_scalar("train/speed", speed, global_step)
+            writer.add_scalar("train/loss", loss.item(), global_step)
+            print(
+                f"speed={speed:.2f}(steps/s), time={int(time_so_far)}(s), loss={loss.item():.3g}",
+                end="\r",
+            )
 
-train(max_seconds=360, warmup_steps=1000)
 
+def validate(train_dl, val_dl, model, optim):
+    print("validating...", end="/r")
+    # schedulefree setup
+    model.train()
+    optim.eval()
+    with torch.no_grad():
+        for imgs, invdepths in itertools.islice(train_dl, 50):
+            model(imgs)
+    model.eval()
+    # validation
+    with torch.no_grad():
+        scores = {"l1": 0}
+        for imgs, invdepths in val_dl:
+            out = model(imgs).unsqueeze(1)
+            scores["l1"] += torch.nn.functional.l1_loss(
+                out, invdepths
+            )  # add mean over batch
+        scores = {k: v / len(val_dl) for k, v in scores.items()}
+    print("done validating.", end="/r")
+    # schedulefree setup
+    model.train()
+    optim.train()
+    return scores
+
+
+if __name__ == "__main__":
+    from fire import Fire
+
+    Fire(train)
